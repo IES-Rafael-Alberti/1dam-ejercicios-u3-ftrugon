b8bb89c5a2359d4371c44ebd0108325416233115
ftrugon <fjtrujillo@iesrafaelalberti.es>
2023-11-13 21:36:50 +0100

    1SDA

--- src/__init__.py
+++ src/__init__.py
@@ -1,1 +1,13 @@
+def pedir_numeros():
+    contnum = 1
+    linea = ""
+    print("Dime los numeros de la primitiva")
+    while contnum <= 6:
+        num = int(input(f"({contnum}) => " ))
+        contnum += 1
+        linea += f"{num} "
 
+#def main():
+    
+if __name__=="__main__":
+    pedir_numeros()
