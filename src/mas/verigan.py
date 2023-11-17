e1b9184fad93bd0b578a0c141875ed6b47eb608e
ftrugon <fjtrujillo@iesrafaelalberti.es>
2023-11-17 17:50:25 +0100

    verigan

---
+++ src/mas/verigan.py
@@ -0,0 +1,13 @@
+def comprobar(tablero):
+    otralista = 
+    for i in range(len(tablero)):
+        if "X" in tablero[i][1]:
+        
+
+
+def main():
+    tablero = (["X","O","O"],["O","X"," "],["X"," ","O"])
+    comprobar(tablero)
+
+if __name__ == "__main__":
+    main()
