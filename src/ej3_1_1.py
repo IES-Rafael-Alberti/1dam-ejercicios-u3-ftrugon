6f2468f18bf4930499fa89b5772085c4444ba277
franconil <franconil04@gmail.com>
2023-11-17 01:40:03 +0100

    buah vaya sacada de rabo

---
+++ src/ej3_1_1.py
@@ -0,0 +1,23 @@
+def num_asig():
+    numasig = int(input("Dime el numero de asignaturas asignadas: "))
+    return(numasig)
+
+def pedir_asignatura(numasig) -> tuple:
+    return tuple(input("Dime una asignatura: ") for i in range(numasig))
+
+def mostrar_asig(asig):
+    mostrar = f"Tus asignaturas son "
+    for i in range(len(asig)):
+        mostrar += f" {asig[i]}"
+    print(mostrar)
+
+def main():
+    try:
+        numasig = num_asig()
+    except ValueError:
+        print("Una asignatura poooorfa")
+    asig = pedir_asignatura(numasig)
+    mostrar_asig(asig)
+
+if __name__ == "__main__":
+    main()
---
+++ src/ej3_1_6.py
@@ -0,0 +1,38 @@
+def num_asig():
+    numasig = int(input("Dime el numero de asignaturas: "))
+    return(numasig)
+
+def pedir_asignotas(numasig:int ) ->list:
+    listatotal = list()
+    for i in range(numasig):
+        asigna = input("Dime una asignatura: ")
+        nota = int(input("Ahora su nota: "))
+        listatotal.append([asigna, nota])
+    return(listatotal)
+
+def quitar(lista:list) -> list:
+    nuevalista = []
+    for i in range(len(lista)):
+        if lista[i][1] >= 5:
+            continue
+        else:
+            nuevalista.append(lista[i])
+    return nuevalista
+
+def mostrar(nuevalista:list) ->str: 
+    linea = "Tienes que repetir "
+    for i in range(len(nuevalista)):
+        if i == 0:
+            linea += f"{nuevalista[i]}"
+        else:
+            linea += f" {nuevalista[i]}"
+    print(linea)
+
+def main():
+    numasig = num_asig()
+    listatotal = pedir_asignotas(numasig)
+    nuevalista = quitar(listatotal)
+    mostrar(nuevalista)
+
+if __name__ == "__main__":
+    main()
