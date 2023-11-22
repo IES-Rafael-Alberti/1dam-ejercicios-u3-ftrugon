3c1d87bca130ae90426afb7a07db61d3b6c948a9
ftrugon <fjtrujillo@iesrafaelalberti.es>
2023-11-22 19:46:23 +0100

    jaja mas dudas

--- src/diccionarios/ej8.py
+++ src/diccionarios/ej8.py
@@ -1,28 +1,41 @@
 def pedir_palabras() -> list:
+    #Le pide al usuario las palabras a traducir
     return input("Dame las palabras traducidas con el formato (palabra1):(traduccion1),(palabra2):(traduccion2): ")
 
-def transformar_a_lista2(listapalabras):
+def transformar_a_lista2(listapalabras:str) -> list:
+    #convierte la cadena str de pedir_palabra a una lista en la que cada valor es [[palabra:traduccion],[palabra2:traduccion]]
     listade2 = listapalabras.split(",")
     return listade2
 
-def lista1(listade2):
-    
-    for i in range(listade2):
-        listade1 = listade2[i].split(":")
-    return listade1
+def transformar_a_diccio(listade2:list) -> dict: 
+    #Para cada elemento en listade2 crea un diccionario en la llave es la posicion antes de ":" y el valor es lo de despues
+    diccio = dict()
+    for elemento in listade2:
+        diccio[elemento.split(":")[0]] = [elemento.split(":")[1]]
+    return diccio
 
-def transformar_a_diccio(listade1):
-    diccio = dict(listade1)
-    print(diccio)
+def pedir_frase() -> str:
+    return input("Dime la frase, yo traducire las palabras: ")
+
+def reemplazar(frase:str, diccio:dict) -> str:
+    listadefrase = frase.split(" ")
+    x =""
+    for elemento in listadefrase:
+        #if elemento in diccio:
+        x += frase.replace(elemento, str(diccio.get(elemento,elemento)))
+    return x
+
 
 def main(): 
     listapalabras = pedir_palabras()
     listade2 = transformar_a_lista2(listapalabras)
-    listade1 = []
-    listade1 = lista1(listade2)
-    diccio = transformar_a_diccio(listade1)
-    print(diccio)
+    diccio = transformar_a_diccio(listade2)
 
-
+    frase = pedir_frase()
+    frasefinal = reemplazar(frase,diccio)
+    if frasefinal =="":
+        print("No se puede traducir ninguna palabra")
+    else:
+        print(frasefinal)
 if __name__ == "__main__":
     main()
